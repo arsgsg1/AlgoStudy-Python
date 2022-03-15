9eb4f2a9572e3604e5c0d512891505b9f2811b17
mickeyshoes <parody9c@gmail.com>
2022-03-15 11:31:10 +0900

    [ADD] initial commit

---
+++ mickeyshoes/1주차/2502.py
@@ -0,0 +1,18 @@
+'''
+A: 첫째날 떡을 준 갯수
+B: 둘째날 떡을 준 갯수
+D: 일수
+K: D일에 떡을 준 갯수
+'''
+
+import sys
+
+input = sys.stdin.readline
+D,K = map(int, input().rstrip('\n').split())
+
+#bottom up
+#1. 점화식 구하기(피보나치)
+
+#2. 브루트포스
+
+#3. A<=B
